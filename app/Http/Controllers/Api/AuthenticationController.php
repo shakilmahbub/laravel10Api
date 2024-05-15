b2c404ff36650c653e5d295a33fed55c0ce82cd0
Syed Mahbubur Rahman <shakilict1@yahoo.com>
2024-05-16 01:04:53 +0600

    added passport authentication with register login and logout

---
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+
+class AuthenticationController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+        $request['password']=Hash::make($request['password']);
+
+        $user = User::create($request->toArray());
+//        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+//        $response = ['token' => $token];
+        return response('Registration successful', 200);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6',
+        ]);
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+
+        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+            // successfull authentication
+            $user = auth()->user();
+
+            $user_token['token'] = $user->createToken('appToken')->accessToken;
+
+            return response()->json([
+                'success' => true,
+                'token' => $user_token,
+                'user' => $user,
+            ], 200);
+        } else {
+            // failure to authenticate
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed to authenticate.',
+            ], 401);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        if (Auth::user()) {
+            $request->user()->token()->revoke();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Logged out successfully',
+            ], 200);
+        }
+    }
+}
